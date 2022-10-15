b412bc07c1b4a0dfd0615e3f8e37a601245b4221
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-10-15 19:13:40 +0430

    Project setup with Tailwind and TS

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        philosopher: ["'Philosopher', sans-serif"],
+      },
+      backgroundImage: {
+        "tickets-img": "url('../public/img/WC.png')",
+      },
+      animation: {
+        text: "text 5s ease infinite",
+      },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "100% 100%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
