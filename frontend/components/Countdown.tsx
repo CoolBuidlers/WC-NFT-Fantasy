437ddff2df84bcc4d821775844fd616cb71cb5c7
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-10-21 07:50:35 +0430

    Added alt prop to remove warning

--- frontend/components/Countdown.tsx
+++ frontend/components/Countdown.tsx
@@ -67,7 +67,7 @@ const Countdown = ({}: Props) => {
           </p>
         </div>
         <div className="absolute md:left-[60%] md:bottom-20 top-[70%] md:top-0 flex justify-center items-center ">
-          <Image src={Ball} width={700} height={700} className="animate-spin" />
+          <Image src={Ball} width={700} height={700} className="animate-spin" alt="spinning-ball" />
         </div>
       </div>
     </section>
