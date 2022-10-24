a946bb971ce681ed5eeba287b398095274b6ae10
YASHURA <61384503+yanukadeneth99@users.noreply.github.com>
2022-10-24 14:06:06 +0530

    Setup Valist Pipeline

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,10 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
