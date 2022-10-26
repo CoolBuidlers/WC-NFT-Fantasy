6817880ad93ea4d247a00ac0358afa64e7cd340d
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-10-26 10:10:22 +0430

    Added faqs data

--- frontend/components/Roadmap.tsx
+++ frontend/components/Roadmap.tsx
@@ -5,17 +5,18 @@ const Roadmap = () => {
     <div>
         {/* Roadmap div with transitions starts here */}
         <div className='flex justify-center'> 
-        <div className="cursor-pointer relative group py-0.5 px-0.5 inline-block">
+        <div className="relative inline-block">
             <div
               className="absolute -inset-1 bg-[#D100D1]
-              to-[#F20089] blur-xl transition-all"
+              to-[#F20089] blur-xl"
               ></div>
-            <button className="relative border-t-4 border-[#D100D1] py-1 transition-all text-white text-3xl lg:text-4xl">
+            <button className="relative border-t-4 border-[#D100D1] py-1 text-white text-3xl lg:text-4xl">
               Roadmap
             </button>
           </div>
         </div>
         {/* Roadmap div with transitions ends here */}
+
     </div>
   )
 }
---
+++ frontend/components/data.tsx
@@ -0,0 +1,39 @@
+// Creating the faqs data in this file so we can map through it later on
+export const faqsData = [
+    {
+      id: 1,
+      question: "How do i link my Metamask wallet?",
+      answer:
+        "It’s just 1 month since i started using Archaos as a web3 learning platform, but the result is very satisfying! I have access to great courses from amazing creators now more  than ever before, thanks to Archaos!",
+    },
+    {
+      id: 2,
+      question: "It’s just incredible!",
+      answer:
+        "It’s just 1 month since i started using Archaos as a web3 learning platform, but the result is very satisfying! I have access to great courses from amazing creators now more  than ever before, thanks to Archaos!",
+    },
+    {
+      id: 3,
+      question: "It’s just incredible!",
+      answer:
+        "It’s just 1 month since i started using Archaos as a web3 learning platform, but the result is very satisfying! I have access to great courses from amazing creators now more  than ever before, thanks to Archaos!",
+    },
+    {
+      id: 4,
+      question: "It’s just incredible!",
+      answer:
+        "It’s just 1 month since i started using Archaos as a web3 learning platform, but the result is very satisfying! I have access to great courses from amazing creators now more  than ever before, thanks to Archaos!",
+    },
+    {
+      id: 5,
+      question: "It’s just incredible!",
+      answer:
+        "It’s just 1 month since i started using Archaos as a web3 learning platform, but the result is very satisfying! I have access to great courses from amazing creators now more  than ever before, thanks to Archaos!",
+    },
+    {
+      id: 6,
+      question: "It’s just incredible!",
+      answer:
+        "It’s just 1 month since i started using Archaos as a web3 learning platform, but the result is very satisfying! I have access to great courses from amazing creators now more  than ever before, thanks to Archaos!",
+    },
+  ];
