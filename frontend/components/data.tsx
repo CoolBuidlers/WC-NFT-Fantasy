87a3fbfddad8191af695189a0766330a92c41cca
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-10-26 10:12:51 +0430

    Added dummy text

--- frontend/components/data.tsx
+++ frontend/components/data.tsx
@@ -4,36 +4,36 @@ export const faqsData = [
       id: 1,
       question: "How do i link my Metamask wallet?",
       answer:
-        "It’s just 1 month since i started using Archaos as a web3 learning platform, but the result is very satisfying! I have access to great courses from amazing creators now more  than ever before, thanks to Archaos!",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.At ac etiam ac sollicitudin lorem in integer quam ac.",
     },
     {
       id: 2,
-      question: "It’s just incredible!",
+      question: "Lorem ipsum dolor sit amet",
       answer:
-        "It’s just 1 month since i started using Archaos as a web3 learning platform, but the result is very satisfying! I have access to great courses from amazing creators now more  than ever before, thanks to Archaos!",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elitAt ac etiam ac sollicitudin lorem in integer quam ac.",
     },
     {
       id: 3,
-      question: "It’s just incredible!",
+      question: "Lorem ipsum dolor sit amet",
       answer:
-        "It’s just 1 month since i started using Archaos as a web3 learning platform, but the result is very satisfying! I have access to great courses from amazing creators now more  than ever before, thanks to Archaos!",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.At ac etiam ac sollicitudin lorem in integer quam ac.",
     },
     {
       id: 4,
-      question: "It’s just incredible!",
+      question: "Lorem ipsum dolor sit amet",
       answer:
-        "It’s just 1 month since i started using Archaos as a web3 learning platform, but the result is very satisfying! I have access to great courses from amazing creators now more  than ever before, thanks to Archaos!",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.At ac etiam ac sollicitudin lorem in integer quam ac.",
     },
     {
       id: 5,
-      question: "It’s just incredible!",
+      question: "Lorem ipsum dolor sit amet",
       answer:
-        "It’s just 1 month since i started using Archaos as a web3 learning platform, but the result is very satisfying! I have access to great courses from amazing creators now more  than ever before, thanks to Archaos!",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.At ac etiam ac sollicitudin lorem in integer quam ac.",
     },
     {
       id: 6,
-      question: "It’s just incredible!",
+      question: "Lorem ipsum dolor sit amet",
       answer:
-        "It’s just 1 month since i started using Archaos as a web3 learning platform, but the result is very satisfying! I have access to great courses from amazing creators now more  than ever before, thanks to Archaos!",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.At ac etiam ac sollicitudin lorem in integer quam ac.",
     },
   ];
