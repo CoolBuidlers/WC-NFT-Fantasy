25ebf6302ccacbfbcc86708d44e065f7b76593f4
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-10-31 18:24:01 +0430

    Added navbar to the NumberGame

--- frontend/pages/NumberGame.tsx
+++ frontend/pages/NumberGame.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import Music from "../public/img/music.jpg";
 import Controller from "../public/img/controller.jpg";
 import Image from "next/image";
+import Navbar from "../components/Navbar";
 type Props = {};
 
 const NumberGame = (props: Props) => {
   return (
     <section className="relative ">
+      <Navbar />
       <div className=" overflow-hidden">
         <div className="absolute top-40 overflow-hidden right-[1030px]">
-          <Image src={Music} alt="MusicImg"/>
+          {/* <Image src={Music} alt="MusicImg"/> */}
         </div>
         <div className="absolute top-20 left-[730px]">
-          <Image src={Controller} alt="controllerImg"/>
+          {/* <Image src={Controller} alt="controllerImg"/> */}
         </div>
       </div>
     </section>
