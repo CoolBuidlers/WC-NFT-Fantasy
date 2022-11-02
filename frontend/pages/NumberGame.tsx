25d4653009087e03a95b37e0429e46ca357acc74
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-11-02 11:24:42 +0430

    Added new images

--- frontend/pages/NumberGame.tsx
+++ frontend/pages/NumberGame.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Music from "../public/img/music.jpg";
+import Music from "../public/img/music.png";
 import Controller from "../public/img/controller.jpg";
 import Image from "next/image";
 import Navbar from "../components/Navbar";
@@ -7,17 +7,23 @@ type Props = {};
 
 const NumberGame = (props: Props) => {
   return (
-    <section className="relative ">
+    <section className="relative">
       <Navbar />
-      <div className=" overflow-hidden">
-        <div className="absolute top-40 overflow-hidden right-[1030px]">
-          {/* <Image src={Music} alt="MusicImg"/> */}
+      
+      <div className="hidden lg:flex justify-between w-full mx-auto">
+        <div className="absolute top-80 overflow-hidden sm:right-[1030px]">
+          <Image src={Music} width={700} height={700} alt="MusicImg"/>
         </div>
-        <div className="absolute top-20 left-[730px]">
-          {/* <Image src={Controller} alt="controllerImg"/> */}
+        <div className="absolute top-80 overflow-hidden sm:left-[1030px]">
+          <Image src={Controller} width={600} height={600} alt="controllerImg"/>
         </div>
       </div>
     </section>
   );
 };
 export default NumberGame;
+
+{/* <div className="absolute hidden sm:flex justify-between">
+  <Image src={Music} width={800} height={800} alt="MusicImg"/>
+  <Image src={Controller} width={800} height={800} alt="Controller"/>
+</div> */}
