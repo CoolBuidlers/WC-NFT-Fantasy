e7889e172c175b9069eb1b1eb2d079d05e18efbb
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-11-02 12:14:20 +0430

    Finished Guessing game page

--- frontend/pages/NumberGame.tsx
+++ frontend/pages/NumberGame.tsx
@@ -1,11 +1,73 @@
-import React from "react";
+import React, {useState} from "react";
 import Music from "../public/img/music.png";
 import Controller from "../public/img/controller.jpg";
 import Image from "next/image";
 import Navbar from "../components/Navbar";
+import incorrectGuessImg from "../public/img/incorrectGuess.png"
 type Props = {};
 
-const NumberGame = (props: Props) => {
+const NumberGame = (props: Props): JSX.Element => {
+
+  const [isStarted, setIsStarted] = useState<boolean>(true);
+  const [joined, setJoined] = useState<boolean>(true);
+  const [hasGuessed, setHasGuesses] = useState<boolean>(true);
+  const [guessedCorrectly, setGuessedCorrectly] = useState<boolean>(true);
+
+  const renderButton = (): JSX.Element | undefined => {
+    if(!isStarted) {
+      return (
+        <div className="flex justify-center cursor-pointer">
+              <span className="play-btn text-center py-4 w-[90%] sm:w-[60%] md:w-[50%] lg:w-[40%] xl:w-[20%] block animate-text cursor-pointer hover:animate-text-hover text-2xl text-white">
+                PlayGame
+              </span>
+          </div>
+      )
+    }
+    else if (isStarted && !joined) {
+      return (
+        <div className="flex justify-center cursor-pointer">
+              <span className="play-btn text-center py-4 w-[90%] sm:w-[60%] md:w-[50%] lg:w-[40%] xl:w-[20%] block animate-text cursor-pointer hover:animate-text-hover text-2xl text-white">
+                JoinGame
+              </span>
+          </div>
+      )
+    }
+    else if (isStarted && joined && !hasGuessed) {
+      return (
+        <div className="flex flex-col items-center justify-center cursor-pointer">
+              <input 
+              className="bg-transparent border-b-4 border-[#DB00B6] mb-10 text-center py-4 w-[90%] sm:w-[50%] md:w-[40%] lg:w-[30%] xl:w-[10%] block animate-text cursor-pointer hover:animate-text-hover text-2xl text-white"
+              type="number"
+              />
+              <span className="play-btn text-center py-4 w-[90%] sm:w-[50%] md:w-[40%] lg:w-[30%] xl:w-[10%] block animate-text cursor-pointer hover:animate-text-hover text-2xl text-white">
+                Sumbit
+              </span>
+          </div>
+      )
+    }
+    else if(isStarted && joined && hasGuessed && !guessedCorrectly) {
+      return(
+        <div className="flex flex-col justify-center items-center">
+          <h3 className="text-white sm:text-xl md:text-2xl py-10">Woops your guess is incorrect Try again Later!</h3>
+          <span className="play-btn text-center py-4 w-[90%] sm:w-[60%] md:w-[50%] lg:w-[40%] xl:w-[20%] block animate-text cursor-pointer hover:animate-text-hover text-2xl text-white">
+                Restart
+          </span>
+        </div>
+      )
+    }
+    else if(isStarted && joined && hasGuessed && guessedCorrectly) {
+      return(
+        <div className="flex flex-col justify-center items-center">
+          <h3 className="text-white sm:text-xl md:text-2xl py-10">Your guess was correct! You have won a Legendary NFT</h3>
+          <h3 className="text-white sm:text-xl md:text-2xl pb-8">Click here to claim your NFT</h3>
+          <span className="play-btn text-center py-4 w-[90%] sm:w-[60%] md:w-[50%] lg:w-[40%] xl:w-[20%] block animate-text cursor-pointer hover:animate-text-hover text-2xl text-white">
+                Claim
+          </span>
+        </div>
+      )
+    }
+  }
+
   return (
     <section className="relative">
       <Navbar />
@@ -28,6 +90,9 @@ const NumberGame = (props: Props) => {
           <Image src={Controller} width={600} height={600} alt="controllerImg"/>
         </div>
       </div>
+      {/* <div className="flex items-center justify-center"> */}
+      {renderButton()}
+      {/* </div> */}
     </section>
   );
 };
