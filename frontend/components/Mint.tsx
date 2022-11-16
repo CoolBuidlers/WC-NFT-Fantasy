2c4914ec998ea86bbac9627499ef23e74055036e
Larry <user@Users-MacBook-Pro.local>
2022-11-15 21:37:11 -0600

    showing message by default

--- frontend/components/Mint.tsx
+++ frontend/components/Mint.tsx
@@ -126,7 +126,7 @@ const Mint = () => {
   }, [address]);
 
   const renderMintComponent = () => {
-    if (hasUserMinted && currentPhase === 0) {
+    if (!hasUserMinted && currentPhase === 0) {
       return (
         <div className="text-white">
           Thank you for choosing your teams! Please wait until next worldcup
