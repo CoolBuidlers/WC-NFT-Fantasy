e4aa625ec82174095ef2ecbe1124ee2c1055ef5b
YASHURA <61384503+yanukadeneth99@users.noreply.github.com>
2022-11-17 00:31:12 +0530

    Worked on the Activity Page

--- frontend/components/PlayersTable.tsx
+++ frontend/components/PlayersTable.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PlayersTable = ({ predictors }: any) => {
   return (
-    <div className="overflow-x-auto relative shadow-2xl shadow-[#8900F2] border-x-[6px] border-t-[6px] border-b-[4px] border-white mx-5 h-[60vh] overflow-y-auto w-2/3">
+    <div className="overflow-x-auto relative shadow-2xl shadow-[#8900F2] border-x-[6px] border-t-[6px] border-b-[4px] border-white mx-5 h-[60vh] overflow-y-auto w-11/12">
       <table className="w-full text-sm text-left text-gray-500">
         <thead className="text-lg text-white uppercase bg-[#8900F2] text-center">
           <tr>
@@ -15,22 +15,34 @@ const PlayersTable = ({ predictors }: any) => {
           </tr>
         </thead>
         <tbody>
-          {predictors?.map((pred: any) => {
-            return (
-              <tr
-                key={pred.id}
-                className="bg-transparent text-white border-b-2 border-white text-center"
-              >
-                <th
-                  scope="row"
-                  className="py-4 px-6 font-medium whitespace-nowrap"
+          {predictors && predictors.length != 0 ? (
+            predictors.map((pred: any) => {
+              return (
+                <tr
+                  key={pred.id}
+                  className="bg-transparent text-white border-b-2 border-white text-center"
                 >
-                  {pred.id}
-                </th>
-                <td className="py-4 px-6">{pred.tokens.length}</td>
-              </tr>
-            );
-          })}
+                  <th
+                    scope="row"
+                    className="py-4 px-6 font-medium whitespace-nowrap"
+                  >
+                    {pred.id}
+                  </th>
+                  <td className="py-4 px-6">{pred.tokens.length}</td>
+                </tr>
+              );
+            })
+          ) : (
+            <tr className="bg-transparent text-white border-b-2 border-white text-center">
+              <th
+                colSpan={2}
+                scope="row"
+                className="py-4 px-6 font-medium whitespace-nowrap"
+              >
+                Nothing to load.. yet
+              </th>
+            </tr>
+          )}
         </tbody>
       </table>
     </div>
