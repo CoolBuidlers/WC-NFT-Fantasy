04f32d2cc6b7ff86cbf5d05576e5f4dab530c715
YASHURA <61384503+yanukadeneth99@users.noreply.github.com>
2022-11-17 01:08:24 +0530

    Setup API to get the countries

--- frontend/components/ActivityTable.tsx
+++ frontend/components/ActivityTable.tsx
@@ -1,6 +1,35 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const ActivityTable = ({ rounds }: { rounds: any[] }) => {
+const ActivityTable = () => {
+  // State to store the countries
+  const [countries, setCountries] = useState<any[]>([]);
+
+  // Get the Data from the URL Query
+  const getData = async (): Promise<void> => {
+    try {
+      const query = `https://soccer.sportmonks.com/api/v2.0/teams/season/18017?api_token=${process.env.NEXT_PUBLIC_API_KEY}`;
+
+      const res = await axios.get(query);
+      console.log(process.env.API_KEY);
+
+      if (res.data.erros) {
+        console.error(res.data.errors);
+        throw new Error("Query Error");
+      }
+
+      setCountries(res.data.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // Runs when the component loads
+  useEffect(() => {
+    getData();
+    console.log("SSS", process.env.NEXT_PUBLIC_API_KEY);
+  }, []);
+
   return (
     <div className="overflow-x-auto w-11/12 relative shadow-2xl shadow-[#8900F2] h-[60vh] overflow-y-auto border-x-[6px] border-t-[6px] border-b-[4px] border-white mx-5">
       <table className="w-full text-sm text-left text-gray-500">
@@ -10,25 +39,25 @@ const ActivityTable = ({ rounds }: { rounds: any[] }) => {
               Country ID
             </th>
             <th scope="col" className="py-3 px-6">
-              Round
+              Name
             </th>
           </tr>
         </thead>
         <tbody>
-          {rounds && rounds.length != 0 ? (
-            rounds.map((round, index) => {
+          {countries.length != 0 ? (
+            countries.map((country) => {
               return (
                 <tr
-                  key={round.id}
+                  key={country.id}
                   className="bg-transparent text-white border-b-2 border-white text-center"
                 >
                   <th
                     scope="row"
                     className="py-4 px-6 font-medium whitespace-nowrap"
                   >
-                    {round.id}
+                    {country.id}
                   </th>
-                  <td className="py-4 px-6">{round.countryIds[index]}</td>
+                  <td className="py-4 px-6">{country.name}</td>
                 </tr>
               );
             })
